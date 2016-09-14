1e9dbb8a947dad9a8cefc6e94c81216327172543
WGXWGX <wgx15145076862@sina.com>
2016-09-14 22:34:03 +0800

    Create arr.js

---
+++ arr.js
@@ -0,0 +1,31 @@
+// var o = {0:"sss",1:"dddd",length:1};
+// var arr = Array.from(o);
+// console.log(arr);
+// var arr = Array.of(3,3,4,5,5,6);
+// arr.fill(10,3,5);
+ // console.log(arr);
+// var arr = new Array(3);
+// var arr = [3];
+//1，起始位置，2，复制起始位置，3，复制结束位置
+// var val = arr.findIndex(()=>{ return "aaa"};
+// function fn(value){
+// 	if(value >3 && value<5){
+// 		return true;
+// 	}else {
+// 		return false;
+// 	}
+// }
+// console.log(val);
+
+
+
+
+
+
+
+
+
+
+
+
+
