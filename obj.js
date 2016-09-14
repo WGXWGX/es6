0f57af7d6ca9413bff49fa2e1c44f1f071109086
WGXWGX <wgx15145076862@sina.com>
2016-09-14 22:39:48 +0800

    Create obj.js

---
+++ obj.js
@@ -0,0 +1,90 @@
+
+
+// var name = "zs";
+// var ok = "OK";
+// var OKid = "sssssss";
+// var o = {
+// 	name,
+// 	[ok+"id"]:"sddd"
+
+// }
+
+// //o[ok] = "dddd";
+
+
+
+// console.log(o);
+
+// console.log(NaN === NaN);
+
+// console.log(-0 === +0);
+
+// console.log(Object.is(-0,+0));
+
+// var arr = [3,4,3,4,NaN];
+
+// var val = arr.findIndex(function(value,key,arr){
+// 	return Object.is(NaN,value);
+// });
+
+// console.log(val);
+
+// var o = {a:3};
+// var oo = {c:5};
+
+// Object.assign(o,{a:2},{b:oo});
+
+// oo.c = 6;
+
+// console.log(o);
+
+// console.log(Object.keys(o));
+
+
+
+
+// var str = "宝宝心里苦";
+
+// //includes() , startsWith() , endsWith()
+// str.includes("宝宝",2);
+// str.startsWith("宝宝",2);
+// console.log(str.endsWith("宝宝",4));
+
+// console.log(str.repeat(3));
+
+
+// var arr = [undefined,4];
+
+// var [a=0,b=0] = arr;
+
+// console.log(a,b);
+
+// var a = 10,b = 20;
+
+// a = [b,b=a][0];
+
+// [a,b] = [b,a];
+
+// console.log(a,b);
+
+
+// var o = {
+// 	a:12,
+// 	b:{c:34},
+// 	d:{c:45}
+// }
+
+// var {b:{c},a} = o;
+
+// console.log(a,c);
+
+
+
+
+
+
+
+
+
+
+
