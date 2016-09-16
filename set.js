96f4b527488de7ea119b74c88741ef39e2b89806
WGXWGX <wgx15145076862@sina.com>
2016-09-16 22:01:10 +0800

    Create set.js

---
+++ set.js
@@ -0,0 +1,51 @@
+
+
+
+//  var arr = [1,2,3,6,5,6,7];
+//  var set = new Set(arr);
+
+// var keys = set.keys();
+// var values = set.values();
+// var entries = set.entries();
+
+// for(entrie of entries){
+// 	console.log(entrie);
+// }
+
+// set.add("ddd");
+// set.add("ddd");
+// set.add("ccc");
+
+// set.delete("ccc");
+
+// set.clear();
+
+// var newarr = Array.from(set);
+
+// console.log(newarr);
+
+// var o = {a:23};
+// var map = new Map();
+
+// map.set(o,123);
+// map.set("bbb",456);
+// map.set("ccc",123);
+
+// map.clear();
+// var keys = map.keys();
+// var values = map.values();
+// var entries = map.entries();
+
+
+// console.log(entries);
+
+// map.forEach(function(value,key){
+// 	console.log(value,key);
+// });
+
+
+
+
+
+
+
