90485b359a596590154fed902e2c4a4623b9f71f
WGXWGX <wgx15145076862@sina.com>
2016-09-16 22:01:59 +0800

    Create test.js

---
+++ test.js
@@ -0,0 +1,28 @@
+
+// {
+
+// 	var a = 0;	
+// }
+
+// for(let i=0;i<10;i++){
+// 	console.log(i);
+// console.log(i);
+// let i = 0;
+var i =0;
+function fn(){
+
+	//console.log(i);
+	let i =1;
+	//var i =2;
+}
+
+fn();
+
+
+
+
+
+
+
+
+
